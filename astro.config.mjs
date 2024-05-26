eadaf61f759b2fad03b615a049003d697cb0296d
Artem Shar <52139984+artemshar@users.noreply.github.com>
2024-05-26 20:04:20 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://artemshar.github.io",
+	site: "https://ailuka.github.io",
 	output: 'static',
   outDir: './dist',
 	integrations: [tailwind()],
